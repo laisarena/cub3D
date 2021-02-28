e47ae7139fac3db98c8c69a5dd7aa8d6768338ec
Lais <laisarena@gmail.com>
2021-03-01 00:32:11 +0100

    Create window

--- srcs/minilibx.c
+++ srcs/minilibx.c
@@ -1,13 +1,23 @@
 #include "cub3d.h"
 
+int	ft_initialize_window(void **mlx, void **window)
+{
+	if (!(*mlx = mlx_init()))
+		ft_error("Error initializing minilibx");
+	if (!(*window = mlx_new_window(*mlx, 500, 500, "cub3D")))
+		ft_error("Error creating window ");
+	return 1;
+}
+
 void	ft_minilibx()
 {
 	void	*mlx;
 	void	*window;
 
-	mlx = mlx_init();
-	window = mlx_new_window(mlx, 500, 500, "cub3D");
-	mlx_pixel_put(mlx, window, 1, 1, 0x00FF0000);
+	mlx = NULL;
+	window = NULL;
+	ft_initialize_window(&mlx, &window);
+	mlx_pixel_put(mlx, window, 100, 100, 0x00FF0000);
 
 	mlx_loop(mlx);
 }
