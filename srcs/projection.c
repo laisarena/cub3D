dd80c61ce60151548ac31df586922cf2b95e63d5
Lais <laisarena@gmail.com>
2021-04-29 02:46:04 +0200

    Collor on ceiling and floor

--- srcs/projection.c
+++ srcs/projection.c
@@ -6,51 +6,100 @@
 /*   By: lfrasson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 02:57:27 by lfrasson          #+#    #+#             */
-/*   Updated: 2021/04/28 03:47:26 by lfrasson         ###   ########.fr       */
+/*   Updated: 2021/04/29 02:43:36 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-typedef struct
+typedef struct s_wall
 {
-	float	perp_distance;
-	float	distance;
 	int		height;
 	int		top;
 	int		bottom;
 }		t_wall;
 
+static int	ft_calc_wall_height(t_ray ray, float player_angle, int win_width)
+{
+	float	perp_distance;
+	float	distance;
+
+	perp_distance = ray.distance * cos(ray.angle - player_angle);
+	distance = (win_width / 2) / tan(FOV_ANGLE / 2);
+	return ((int)((TILE / perp_distance) * distance));
+}
+
+static int	ft_calc_wall_top(int win_hight, int wall_height)
+{
+	int	top;
+
+	top = (win_hight / 2) - (wall_height / 2);
+	if (top < 0)
+		return (0);
+	return (top);
+}
+
+static int	ft_calc_wall_bottom(int win_height, int wall_height)
+{
+	int	bottom;
+
+	bottom = (win_height / 2) + (wall_height / 2);
+	if (bottom > win_height)
+		return (win_height);
+	return (bottom);
+}
+
+static void	ft_render_ceiling(t_wall wall, t_vars *vars, int x)
+{
+	int	y;
+
+	y = 0;
+	while (y < wall.top)
+		ft_put_pixel(&vars->image, x, y++, 0xFF222222);
+}
+
+static void	ft_render_floor(t_wall wall, t_vars *vars, int x)
+{
+	int	y;
+
+	y = wall.bottom;
+	while (y < vars->scene_description.resolution.y)
+		ft_put_pixel(&vars->image, x, y++, 0xFF555555);
+}
+
+static void	ft_render_wall(t_wall wall, t_ray ray, t_vars *vars, int x)
+{
+	int	y;
+
+	y = wall.top;
+	while (y < wall.bottom)
+	{
+		if (ray.is_hit_vertical)
+			ft_put_pixel(&vars->image, x, y, 0xFFFFFFFF);
+		else
+			ft_put_pixel(&vars->image, x, y, 0xFFCCCCCC);
+		y++;
+	}
+}
+
 void	ft_render_3d_projection(t_vars *vars)
 {
-	int i;
-	int	y;
+	int		x;
 	t_wall	wall;
 
-	i = 0;
-	while(i < vars->scene_description.resolution.x)
+	x = 0;
+	while (x < vars->scene_description.resolution.x)
 	{
-		wall.perp_distance = vars->ray[i].distance
-			* cos(vars->ray[i].angle - vars->player.rotation_angle);
-		wall.distance = (vars->scene_description.resolution.x / 2)
-			/ tan(FOV_ANGLE / 2);
-		wall.height = (int)((TILE / wall.perp_distance) * wall.distance);
-		wall.top = (vars->scene_description.resolution.y / 2) - (wall.height / 2);
-		if (wall.top < 0)
-			wall.top = 0;
-		wall.bottom = (vars->scene_description.resolution.y / 2) + (wall.height / 2);
-		if (wall.bottom > vars->scene_description.resolution.y)
-		wall.bottom = vars->scene_description.resolution.y;
-		y = wall.top;
-		while(y < wall.bottom)
-		{
-			if (vars->ray[i].is_hit_vertical)
-				ft_put_pixel(&vars->image, i, y, 0xFFFFFFFF);
-			else
-				ft_put_pixel(&vars->image, i, y, 0xFFCCCCCC);
-			y++;
-		}
-		i++;
+		wall.height = ft_calc_wall_height(vars->ray[x],
+				vars->player.rotation_angle,
+				vars->scene_description.resolution.x);
+		wall.top = ft_calc_wall_top(vars->scene_description.resolution.y,
+				wall.height);
+		wall.bottom = ft_calc_wall_bottom(vars->scene_description.resolution.y,
+				wall.height);
+		ft_render_ceiling(wall, vars, x);
+		ft_render_wall(wall, vars->ray[x], vars, x);
+		ft_render_floor(wall, vars, x);
+		x++;
 	}
-
 }
